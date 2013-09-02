48f69928cfe063958e15bbc4358aa89d1829e8bc
Julian Nadeau <julian@jnadeau.ca>
2013-09-01 18:00:27 -0700

    Settings

---
+++ OCTranspo/OCTranspo/Views/Settings.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using OCTranspo;
+
+namespace OCTranspo.Views
+{
+    public partial class Settings : PhoneApplicationPage
+    {
+        string distanceString;
+        int distance;
+
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void nearbyDistanceSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Slider nearbySlider = (Slider)sender;
+            distance = (int)nearbySlider.Value;
+            distanceString = distance.ToString() + "m";
+        }
+
+        private void ApplicationBarMenuItem_Click_2(object sender, EventArgs e)
+        {
+            Navigation.NavigateToInfo();
+        }
+
+        private void ApplicationBarMenuItem_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
